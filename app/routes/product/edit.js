569988db0a34c2d5a7656d61af054560e8d25b8b
lalaingampus <bottlewar@gmail.com>
2022-04-04 08:58:44 +0700

    Done product

--- app/routes/product/edit.js
+++ app/routes/product/edit.js
@@ -2,15 +2,17 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   templateName: 'product/create',
-  model() {
-    let result = {
-      id: 1,
-      name: 'Siemens',
-      description: 'Handphone',
-      purchasePrice: 150000,
-      sellPrice: 500000,
-      isPolyphonic: true,
-    };
-    return result;
+  model({id}) {
+//    let result = {
+//      id: 1,
+//      name: 'Siemens',
+//      description: 'Handphone',
+//      purchasePrice: 150000,
+//      sellPrice: 500000,
+//      isPolyphonic: true,
+//    };
+//    return result;
+
+  return this.store.peekRecord('product', id);
   },
 });
