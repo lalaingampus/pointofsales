e81d704c46e61f52ef78d14bca8bc6c56bea6838
lalaingampus <bottlewar@gmail.com>
2022-04-06 11:14:06 +0700

    add paging

--- app/routes/product/index.js
+++ app/routes/product/index.js
@@ -1,7 +1,30 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  model() {
-    return this.store.findAll('product');
+  queryParams: {
+   page: {
+    refreshModel: true
+   },
+   size: {
+    refreshModel: true
+   },
+   filter: {
+    refreshModel: true
+   },
+   sort: {
+    refreshModel: true
+   },
+  },
+
+  model(params) {
+    // return this.store.findAll('product');
+    return this.store.query('product', {
+     page: {
+      number: params.page,
+      size: params.page,
+     },
+     filter: params.filter,
+     sort: params.sort
+    });
   },
 });
